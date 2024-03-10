236a6666c224b23b93a9a2ed62cbc1ce5da25677
Wesley <wesmith314@gmail.com>
2024-03-10 12:21:46 -0700

    formats added paragraphs

--- src/app/page.js
+++ src/app/page.js
@@ -91,7 +91,7 @@ const DesktopContainer = ({ children }) => {
               style={{
                 backgroundColor: fixed ? "#D2C5B3" : "rgba(255, 255, 255, .15)",
                 justifyContent: "center",
-                fontFamily: fixed ? "Bask-Cursive" : "Baskerville"
+                fontFamily: fixed ? "Bask-Cursive" : "Baskerville",
               }}
             >
               <Container style={{ display: "flex", justifyContent: "center" }}>
@@ -204,14 +204,23 @@ ResponsiveContainer.propTypes = {
 
 const Home = () => (
   <ResponsiveContainer>
-    <Segment style={{ padding: "8em 0em", backgroundColor: "#F2EDE3" }} vertical>
+    <Segment
+      style={{ padding: "8em 0em", backgroundColor: "#F2EDE3" }}
+      vertical
+    >
       <Grid container stackable verticalAlign="middle">
         <Grid.Row>
           <Grid.Column width={8}>
-            <Header as="h3" style={{ fontSize: "5em", fontFamily: "Fancy"}}>
+            <Header as="h3" style={{ fontSize: "5em", fontFamily: "Fancy" }}>
               Welcome to Vintage Vista
             </Header>
-            <p style={{ fontSize: "1.5em", fontFamily: "Baskerville", color: "#5C5C5C" }}>
+            <p
+              style={{
+                fontSize: "1.5em",
+                fontFamily: "Baskerville",
+                color: "#5C5C5C",
+              }}
+            >
               Nestled in the rolling hills of Napa Valley, our family-owned
               winery has been crafting award-winning wines since{" "}
               <strong>1972</strong>. Our passion for winemaking is deeply rooted
@@ -221,7 +230,13 @@ const Home = () => (
             <Header as="h3" style={{ fontSize: "5em", fontFamily: "Fancy" }}>
               Our Philosophy
             </Header>
-            <p style={{ fontSize: "1.5em", fontFamily: "Baskerville", color: "#5C5C5C" }}>
+            <p
+              style={{
+                fontSize: "1.5em",
+                fontFamily: "Baskerville",
+                color: "#5C5C5C",
+              }}
+            >
               At Vineyard Valley, we believe great wine starts in the vineyard.
               Our winemaker, James Wilkins, works closely with our viticulture
               team to cultivate the finest grapes that truly express the unique
@@ -231,16 +246,14 @@ const Home = () => (
             </p>
           </Grid.Column>
           <Grid.Column floated="right" width={6}>
-            <Image
-            className="grapes"
-              alt="grapes"
-              src="/vines.jpg"
-            />
+            <Image className="grapes" alt="grapes" src="/vines.jpg" />
           </Grid.Column>
         </Grid.Row>
         <Grid.Row>
           <Grid.Column textAlign="center">
-            <Button size="huge" className="more-btn" >More About Us</Button>
+            <Button size="huge" className="btn">
+              More About Us
+            </Button>
           </Grid.Column>
         </Grid.Row>
       </Grid>
@@ -250,38 +263,40 @@ const Home = () => (
       <Grid celled="internally" columns="equal" stackable>
         <Grid.Row textAlign="center">
           <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              What a Company
+            <Header as="h3" style={{ fontSize: "5em", fontFamily: "Fancy" }}>
+              Join Our Wine Club!
             </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              That is what they all say about us
+            <p style={{ fontSize: "1.33em", fontFamily: "Baskerville" }}>
+              Members receive exclusive access to limited production wines,
+              invitations to events, and generous discounts. Can't make it to
+              the winery? Browse and purchase our exceptional wines online for
+              delivery to your door.
             </p>
           </Grid.Column>
           <Grid.Column style={{ paddingBottom: "5em", paddingTop: "5em" }}>
-            <Header as="h3" style={{ fontSize: "2em" }}>
-              I shouldnt have gone with their competitor.
+            <Header as="h3" style={{ fontSize: "5em", fontFamily: "Fancy" }}>
+              I shouldnt have gone with their competitor
             </Header>
-            <p style={{ fontSize: "1.33em" }}>
-              <Image avatar alt="vineyard" src="/vineyard2.jpg" />
-              <b>Nan</b> Chief Fun Officer Acme Toys
-            </p>
           </Grid.Column>
         </Grid.Row>
       </Grid>
     </Segment>
 
-    <Segment style={{ padding: "8em 0em", backgroundColor: "#F2EDE3" }} vertical>
+    <Segment
+      style={{ padding: "8em 0em", backgroundColor: "#F2EDE3" }}
+      vertical
+    >
       <Container text>
-        <Header as="h3" style={{ fontSize: "2em" }}>
-          Breaking The Grid, Grabs Your Attention
+        <Header as="h3" style={{ fontSize: "5em", fontFamily: "Fancy" }}>
+          Join Us for a Tasting
         </Header>
-        <p style={{ fontSize: "1.33em" }}>
-          Instead of focusing on content creation and hard work, we have learned
-          how to master the art of doing nothing by providing massive amounts of
-          whitespace and generic content that can seem massive, monolithic and
-          worth your attention.
+        <p style={{ fontSize: "1.33em", fontFamily: "Baskerville" }}>
+          Visit our gorgeous Tuscan-style tasting room and sip your way through
+          our current release wines. Our friendly and knowledgeable staff will
+          guide you through the tasting experience as you savor each pour. Stay
+          for a tour of our vineyards and production facility.
         </p>
-        <Button as="a" size="large">
+        <Button as="a" size="large" className="btn">
           Read More
         </Button>
 
@@ -294,21 +309,24 @@ const Home = () => (
           <a href="#">Case Studies</a>
         </Divider>
 
-        <Header as="h3" style={{ fontSize: "2em" }}>
+        <Header as="h3" style={{ fontSize: "5em", fontFamily: "Fancy" }}>
           Did We Tell You About Our Bananas?
         </Header>
-        <p style={{ fontSize: "1.33em" }}>
+        <p style={{ fontSize: "1.33em", fontFamily: "Baskerville" }}>
           Yes I know you probably disregarded the earlier boasts as non-sequitur
           filler content, but its really true. It took years of gene splicing
           and combinatory DNA research, but our bananas can really dance.
         </p>
-        <Button as="a" size="large">
+        <Button as="a" size="large" className="btn">
           Im Still Quite Interested
         </Button>
       </Container>
     </Segment>
 
-    <Segment vertical style={{ padding: "5em 0em", backgroundColor: "#D2C5B3" }}>
+    <Segment
+      vertical
+      style={{ padding: "5em 0em", backgroundColor: "#D2C5B3" }}
+    >
       <Container>
         <Grid divided stackable>
           <Grid.Row>
