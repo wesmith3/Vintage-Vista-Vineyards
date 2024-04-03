ca5513446ed7aaac593bb08eb596af11e4198b93
Wesley <wesmith314@gmail.com>
2024-04-02 23:22:32 -0700

    adds first wine card

--- src/app/shop/page.js
+++ src/app/shop/page.js
@@ -1,7 +1,7 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
 import {
-  Button,
+  Card,
   Container,
   Grid,
   Header,
@@ -13,31 +13,48 @@ import {
   Segment,
   Step,
   Table,
-} from 'semantic-ui-react'
+} from "semantic-ui-react";
 
 const style = {
   h1: {
-    marginTop: '3em',
+    marginTop: "3em",
   },
   h2: {
-    margin: '4em 0em 2em',
+    margin: "4em 0em 2em",
     fontFamily: "Fancy",
   },
   h3: {
-    marginTop: '2em',
-    padding: '2em 0em',
+    marginTop: "2em",
+    padding: "2em 0em",
   },
   last: {
-    marginBottom: '300px',
+    marginBottom: "300px",
   },
-}
+};
 
 const Page = () => (
   <div>
-    <Header as='h3' content='Wines' style={style.h3} textAlign='center' />
+    <Header as="h3" content="Wines" style={style.h3} textAlign="center" />
     <Grid columns={3} container>
       <Grid.Column>
-        <Segment>Merlot</Segment>
+          <Card>
+            <Image src="/images/avatar/large/matthew.png" wrapped ui={false} />
+            <Card.Content>
+              <Card.Header>Matthew</Card.Header>
+              <Card.Meta>
+                <span className="date">Joined in 2015</span>
+              </Card.Meta>
+              <Card.Description>
+                Matthew is a musician living in Nashville.
+              </Card.Description>
+            </Card.Content>
+            <Card.Content extra>
+              <a>
+                <Icon name="user" />
+                22 Friends
+              </a>
+            </Card.Content>
+          </Card>
       </Grid.Column>
       <Grid.Column>
         <Segment>Syrah</Segment>
@@ -55,8 +72,7 @@ const Page = () => (
         <Segment>Chardonnay</Segment>
       </Grid.Column>
     </Grid>
-
   </div>
-)
+);
 
-export default Page
+export default Page;
