1fb11e2a1de08a063fca4b7f0f259439688af925
Wesley <wesmith314@gmail.com>
2024-09-06 07:48:22 -0700

    adds homepage heading component

---
+++ src/app/HompageHeading.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Container, Header } from "semantic-ui-react";
+import { COLORS, FONTS } from '../utils/constants';
+
+const HomepageHeading = ({ mobile }) => (
+  <Container text>
+    <Header
+      as="h1"
+      content="Vintage Vista Vineyards"
+      inverted
+      style={{
+        fontSize: mobile ? "2em" : "10em",
+        fontWeight: "bolder",
+        marginBottom: 0,
+        marginTop: mobile ? "1.5em" : ".25em",
+        fontFamily: FONTS.FANCY,
+        color: COLORS.WHITE,
+      }}
+      className="text-stroke"
+      data-text="Vintage Vista Vineyards"
+    />
+  </Container>
+);
+
+export default HomepageHeading;
