5e42d3f1661d1b9f9c709b48ccd78838f4e2817b
Wesley <wesmith314@gmail.com>
2024-09-06 07:35:48 -0700

    removes footer from page

--- src/app/page.js
+++ src/app/page.js
@@ -361,42 +361,6 @@ const Home = () => (
       </Container>
     </Segment>
 
-    <Segment
-      vertical
-      style={{ padding: "5em 0em", backgroundColor: "#D2C5B3" }}
-    >
-      <Container>
-        <Grid divided stackable>
-          <Grid.Row>
-            <Grid.Column width={3}>
-              <Header as="h4" content="About" />
-              <List link>
-                <List.Item as="a">Sitemap</List.Item>
-                <List.Item as="a">Contact Us</List.Item>
-                <List.Item as="a">Religious Ceremonies</List.Item>
-                <List.Item as="a">Gazebo Plans</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Header as="h4" content="Services" />
-              <List link>
-                <List.Item as="a">Online Shopping</List.Item>
-                <List.Item as="a">Wine Club</List.Item>
-                <List.Item as="a">Event Calender</List.Item>
-                <List.Item as="a">Cafe Menu</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={7}>
-              <Header as="h4">Footer Header</Header>
-              <p>
-                Extra space for a call to action inside the footer that could
-                help re-engage users.
-              </p>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
-      </Container>
-    </Segment>
   </ResponsiveContainer>
 );
 
